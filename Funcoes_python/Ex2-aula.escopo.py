39e9bb68d087999c879efe6ce5a67201d3a6a546
julianadsena <104790519+julianadsena@users.noreply.github.com>
2022-12-02 11:01:34 -0300

    Add files via upload

---
+++ Funcoes_python/Ex2-aula.escopo.py
@@ -0,0 +1,6 @@
+"""Faça uma função que informe a quantidade de dígitos de um
+determinado número inteiro informado."""
+def quant(numero):
+    return len(numero)
+num1=input('Digite um número:')
+print(quant(num1))
