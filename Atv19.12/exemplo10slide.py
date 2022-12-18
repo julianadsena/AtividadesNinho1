c858628826caa45898460b0eb238ad85947e03b3
julianadsena <104790519+julianadsena@users.noreply.github.com>
2022-12-18 20:41:24 -0300

    Delete Atv19.12 directory

--- Atv19.12/exemplo10slide.py
+++
@@ -1,9 +0,0 @@
-#algoritmo numeros impares de 100 a 200
-n = 100
-lista = []
-while n <= 200:
-    n = n + 1
-    if (n%2) != 0:        
-        lista.append(n)        
-print(lista)
-
--- Atv19.12/exemplo1slide.py
+++
@@ -1,6 +0,0 @@
-#algoritmo par ou impar 
-n = int(input('Digite um número inteiro:'))
-if (n%2)==0:
-    print(f'O número {n} é par')
-else:
-    print(f'O número {n} é ímpar')
